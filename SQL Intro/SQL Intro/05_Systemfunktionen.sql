e04e820b69d1742437a2cc8636aa84a86e7abb2d
Rauchendraser <andreasr@ppedv.de>
2024-11-12 14:30:43 +0100

    LEFT  RIGHT INNER JOIN VIew

--- SQL Intro/SQL Intro/05_Systemfunktionen.sql
+++ SQL Intro/SQL Intro/05_Systemfunktionen.sql
@@ -15,3 +15,45 @@ select year(orderdate),* from orders where year(orderdate) =1997
 --zu einem Datum etwas dazurechen
 select dateadd(dd,100,getdate())
 
+--ww 
+--hh Stunden
+--ss Sekunden
+--yy Jahr
+--mm Monat
+--qq Quartal
+--dw day of week
+
+
+select datepart(qq,getdate()), datepart(ww, getdate())+1
+
+select 
+			datepart(qq, orderdate) , orderdate
+from orders
+where
+		datepart(qq, orderdate) = 4
+
+
+select * from orders
+
+
+--wieviel Tage zu spät oder zu früh kam eine Bestellung an
+--negativ sollte zu früh sein... 
+--sortiere nach den zu spät angekommenen
+
+select	  orderid, shippeddate, requireddate
+		, datediff(dd, shippeddate, requireddate) as zuspaet
+from	 
+		 orders
+where 
+		datediff(dd, shippeddate, requireddate)< 0
+order by 
+		 zuspaet
+
+
+select datename(dw,getdate())
+
+select EOMONTH(getdate())-- letzter Tag des Monats
+
+
+
+
